f83f8865b4782622e8c8f950b15a89ede6e96637
Sofia Kargina <sonya.kargina@mail.ru>
2024-10-08 10:30:51 +0400

    Custon Table View Cell

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces2
+//
+//  Created by Софья  on 08.10.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyPlaces2/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces2
+//
+//  Created by Софья  on 08.10.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+  
+
+}
